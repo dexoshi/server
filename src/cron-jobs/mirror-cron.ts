19869854cda3990c97b20d8b7aa514a7cc97ed20
Charlie Blackstock <charlieblackstock@gmail.com>
2023-09-28 09:37:12 -0600

    fix: mirror cron

--- src/cron-jobs/mirror-cron.ts
+++ src/cron-jobs/mirror-cron.ts
@@ -1,10 +1,11 @@
 import cron from '@elysiajs/cron'
-import { eq } from 'drizzle-orm'
 import Elysia from 'elysia'
 import { db } from '../db/db'
 import { cache } from '../db/schema'
 import { getWhoMirroredPublication } from '../mirrors'
 import { addToMintQueue } from '../queue'
+
+const MINT_PUBLICATION_IDS = ['0x91ac-0x01-DA-6709cc0d']
 
 export function init(app: Elysia) {
   app.use(
@@ -14,51 +15,55 @@ export function init(app: Elysia) {
       run: async () => {
         console.log('🕐 Checking Mirrors...')
 
-        let cursor = await db.select().from(cache).where(eq(cache.key, 'mirrors')).limit(1)
-
-        const parsedValue = cursor[0]?.value
-          ? (JSON.parse(cursor[0].value) as { publicationId: string; lastCursor: string })
-          : null
-
-        const formattedCursor = parsedValue?.lastCursor
-          ? JSON.stringify(JSON.parse(parsedValue.lastCursor))
-          : undefined
-        console.log('🚀 ~ file: mirror-cron.ts:24 ~ run: ~ formattedCursor:', formattedCursor)
-
-        const mirrors = await getWhoMirroredPublication({
-          publicationId: '0x91ac-0x01-DA-6709cc0d',
-          cursor: formattedCursor,
-        })
-
-        console.log('Mirrors:', mirrors.items.length, mirrors.pageInfo.next)
-        await Promise.all(
-          mirrors.items.map((m) => {
-            return addToMintQueue({
-              profile: m,
-              publicationId: '0x91ac-0x01-DA-6709cc0d',
-              type: 'mint',
-            })
+        for await (const publicationId of MINT_PUBLICATION_IDS) {
+          const mirrors = await getWhoMirroredPublication({
+            publicationId,
           })
-        )
 
-        try {
-          if (!mirrors.pageInfo.next) return
-          const value = JSON.stringify({
-            publicationId: '0x91ac-0x01-DA-6709cc0d',
-            lastCursor: mirrors.pageInfo.next,
+          const alreadyMinted = await db.query.mintedCards.findMany({
+            where: (m, { inArray, and, eq }) =>
+              and(
+                eq(m.publicationId, publicationId),
+                inArray(
+                  m.profileId,
+                  mirrors.items.map((m) => m.id)
+                )
+              ),
           })
-          await db
-            .insert(cache)
-            .values({
-              key: 'mirrors',
-              value,
+
+          const alreadyMintedProfileIds = alreadyMinted.map((m) => m.profileId)
+
+          await Promise.all(
+            mirrors.items
+              .filter((m) => !alreadyMintedProfileIds.includes(m.id))
+              .map((m) => {
+                return addToMintQueue({
+                  profile: m,
+                  publicationId,
+                  type: 'mint',
+                })
+              })
+          )
+
+          try {
+            if (!mirrors.pageInfo.next) return
+            const value = JSON.stringify({
+              publicationId,
+              lastCursor: mirrors.pageInfo.next,
             })
-            .onConflictDoUpdate({
-              target: cache.key,
-              set: { value },
-            })
-        } catch (e) {
-          console.error(e)
+            await db
+              .insert(cache)
+              .values({
+                key: 'mirrors',
+                value,
+              })
+              .onConflictDoUpdate({
+                target: cache.key,
+                set: { value },
+              })
+          } catch (e) {
+            console.error(e)
+          }
         }
       },
     })
--- src/mirrors.ts
+++ src/mirrors.ts
@@ -1,16 +1,8 @@
 import { lensClient } from './lens-client'
 
-export const getWhoMirroredPublication = async ({
-  publicationId,
-  cursor,
-}: {
-  publicationId: string
-  cursor?: string
-}) => {
-  const result = await lensClient.profile.fetchAll({
+export const getWhoMirroredPublication = async ({ publicationId }: { publicationId: string }) => {
+  return lensClient.profile.fetchAll({
     whoMirroredPublicationId: publicationId,
     limit: 50,
-    cursor,
   })
-  return result
 }
